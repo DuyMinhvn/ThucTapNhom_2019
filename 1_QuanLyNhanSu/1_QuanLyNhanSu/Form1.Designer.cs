1b4530b7501208d0bdc54ccdb6d228492fabf623
buimanhhoai <54977879+buimanhhoai@users.noreply.github.com>
2019-09-20 15:14:06 +0700

    form

--- 1_QuanLyNhanSu/1_QuanLyNhanSu/Form1.Designer.cs
+++ 1_QuanLyNhanSu/1_QuanLyNhanSu/Form1.Designer.cs
@@ -30,23 +30,24 @@
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button_them = new System.Windows.Forms.Button();
-            this.button_sửa = new System.Windows.Forms.Button();
-            this.button_xoa = new System.Windows.Forms.Button();
-            this.button_timkiem = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.comboBox_gioitinh = new System.Windows.Forms.ComboBox();
-            this.dateTimePicker_ngaysinh = new System.Windows.Forms.DateTimePicker();
-            this.textBox_diachi = new System.Windows.Forms.TextBox();
-            this.textBox_hoten = new System.Windows.Forms.TextBox();
-            this.textBox_dantoc = new System.Windows.Forms.TextBox();
-            this.textBox_sdt = new System.Windows.Forms.TextBox();
             this.button_luu = new System.Windows.Forms.Button();
+            this.textBox_sdt = new System.Windows.Forms.TextBox();
+            this.textBox_dantoc = new System.Windows.Forms.TextBox();
+            this.textBox_hoten = new System.Windows.Forms.TextBox();
+            this.textBox_diachi = new System.Windows.Forms.TextBox();
+            this.dateTimePicker_ngaysinh = new System.Windows.Forms.DateTimePicker();
+            this.comboBox_gioitinh = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_timkiem = new System.Windows.Forms.Button();
+            this.button_xoa = new System.Windows.Forms.Button();
+            this.button_sửa = new System.Windows.Forms.Button();
+            this.button_them = new System.Windows.Forms.Button();
+            this.button_huongdan = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -62,6 +63,7 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.button_huongdan);
             this.panel1.Controls.Add(this.button_luu);
             this.panel1.Controls.Add(this.textBox_sdt);
             this.panel1.Controls.Add(this.textBox_dantoc);
@@ -85,95 +87,49 @@
             this.panel1.Size = new System.Drawing.Size(762, 431);
             this.panel1.TabIndex = 2;
             // 
-            // button_them
+            // button_luu
             // 
-            this.button_them.Location = new System.Drawing.Point(33, 15);
-            this.button_them.Name = "button_them";
-            this.button_them.Size = new System.Drawing.Size(75, 23);
-            this.button_them.TabIndex = 2;
-            this.button_them.Text = "Thêm";
-            this.button_them.UseVisualStyleBackColor = true;
+            this.button_luu.Location = new System.Drawing.Point(402, 14);
+            this.button_luu.Name = "button_luu";
+            this.button_luu.Size = new System.Drawing.Size(75, 23);
+            this.button_luu.TabIndex = 18;
+            this.button_luu.Text = "Lưu";
+            this.button_luu.UseVisualStyleBackColor = true;
             // 
-            // button_sửa
+            // textBox_sdt
             // 
-            this.button_sửa.Location = new System.Drawing.Point(152, 14);
-            this.button_sửa.Name = "button_sửa";
-            this.button_sửa.Size = new System.Drawing.Size(75, 23);
-            this.button_sửa.TabIndex = 3;
-            this.button_sửa.Text = "Sửa";
-            this.button_sửa.UseVisualStyleBackColor = true;
+            this.textBox_sdt.Location = new System.Drawing.Point(128, 213);
+            this.textBox_sdt.Name = "textBox_sdt";
+            this.textBox_sdt.Size = new System.Drawing.Size(161, 20);
+            this.textBox_sdt.TabIndex = 17;
             // 
-            // button_xoa
+            // textBox_dantoc
             // 
-            this.button_xoa.Location = new System.Drawing.Point(278, 14);
-            this.button_xoa.Name = "button_xoa";
-            this.button_xoa.Size = new System.Drawing.Size(75, 23);
-            this.button_xoa.TabIndex = 4;
-            this.button_xoa.Text = "Xóa";
-            this.button_xoa.UseVisualStyleBackColor = true;
+            this.textBox_dantoc.Location = new System.Drawing.Point(128, 107);
+            this.textBox_dantoc.Name = "textBox_dantoc";
+            this.textBox_dantoc.Size = new System.Drawing.Size(161, 20);
+            this.textBox_dantoc.TabIndex = 16;
             // 
-            // button_timkiem
+            // textBox_hoten
             // 
-            this.button_timkiem.Location = new System.Drawing.Point(529, 14);
-            this.button_timkiem.Name = "button_timkiem";
-            this.button_timkiem.Size = new System.Drawing.Size(75, 23);
-            this.button_timkiem.TabIndex = 5;
-            this.button_timkiem.Text = "Tìm kiếm";
-            this.button_timkiem.UseVisualStyleBackColor = true;
+            this.textBox_hoten.Location = new System.Drawing.Point(128, 64);
+            this.textBox_hoten.Name = "textBox_hoten";
+            this.textBox_hoten.Size = new System.Drawing.Size(161, 20);
+            this.textBox_hoten.TabIndex = 15;
             // 
-            // label1
+            // textBox_diachi
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(33, 64);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(39, 13);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Họ tên";
+            this.textBox_diachi.Location = new System.Drawing.Point(430, 107);
+            this.textBox_diachi.Name = "textBox_diachi";
+            this.textBox_diachi.Size = new System.Drawing.Size(200, 20);
+            this.textBox_diachi.TabIndex = 14;
             // 
-            // label2
+            // dateTimePicker_ngaysinh
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(344, 64);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(54, 13);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "Ngày sinh";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(33, 107);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(45, 13);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "Dân tộc";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(33, 157);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(47, 13);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "Giới tính";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(344, 107);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(54, 13);
-            this.label5.TabIndex = 10;
-            this.label5.Text = "Quê quán";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(36, 213);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(29, 13);
-            this.label6.TabIndex = 11;
-            this.label6.Text = "SDT";
+            this.dateTimePicker_ngaysinh.Location = new System.Drawing.Point(430, 64);
+            this.dateTimePicker_ngaysinh.Name = "dateTimePicker_ngaysinh";
+            this.dateTimePicker_ngaysinh.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker_ngaysinh.TabIndex = 13;
             // 
             // comboBox_gioitinh
             // 
@@ -186,49 +142,104 @@
             this.comboBox_gioitinh.Size = new System.Drawing.Size(161, 21);
             this.comboBox_gioitinh.TabIndex = 12;
             // 
-            // dateTimePicker_ngaysinh
+            // label6
             // 
-            this.dateTimePicker_ngaysinh.Location = new System.Drawing.Point(430, 64);
-            this.dateTimePicker_ngaysinh.Name = "dateTimePicker_ngaysinh";
-            this.dateTimePicker_ngaysinh.Size = new System.Drawing.Size(200, 20);
-            this.dateTimePicker_ngaysinh.TabIndex = 13;
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(36, 213);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "SDT";
             // 
-            // textBox_diachi
+            // label5
             // 
-            this.textBox_diachi.Location = new System.Drawing.Point(430, 107);
-            this.textBox_diachi.Name = "textBox_diachi";
-            this.textBox_diachi.Size = new System.Drawing.Size(200, 20);
-            this.textBox_diachi.TabIndex = 14;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(344, 107);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Quê quán";
             // 
-            // textBox_hoten
+            // label4
             // 
-            this.textBox_hoten.Location = new System.Drawing.Point(128, 64);
-            this.textBox_hoten.Name = "textBox_hoten";
-            this.textBox_hoten.Size = new System.Drawing.Size(161, 20);
-            this.textBox_hoten.TabIndex = 15;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Giới tính";
             // 
-            // textBox_dantoc
+            // label3
             // 
-            this.textBox_dantoc.Location = new System.Drawing.Point(128, 107);
-            this.textBox_dantoc.Name = "textBox_dantoc";
-            this.textBox_dantoc.Size = new System.Drawing.Size(161, 20);
-            this.textBox_dantoc.TabIndex = 16;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Dân tộc";
             // 
-            // textBox_sdt
+            // label2
             // 
-            this.textBox_sdt.Location = new System.Drawing.Point(128, 213);
-            this.textBox_sdt.Name = "textBox_sdt";
-            this.textBox_sdt.Size = new System.Drawing.Size(161, 20);
-            this.textBox_sdt.TabIndex = 17;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(344, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Ngày sinh";
             // 
-            // button_luu
+            // label1
             // 
-            this.button_luu.Location = new System.Drawing.Point(402, 14);
-            this.button_luu.Name = "button_luu";
-            this.button_luu.Size = new System.Drawing.Size(75, 23);
-            this.button_luu.TabIndex = 18;
-            this.button_luu.Text = "Lưu";
-            this.button_luu.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Họ tên";
+            // 
+            // button_timkiem
+            // 
+            this.button_timkiem.Location = new System.Drawing.Point(529, 14);
+            this.button_timkiem.Name = "button_timkiem";
+            this.button_timkiem.Size = new System.Drawing.Size(75, 23);
+            this.button_timkiem.TabIndex = 5;
+            this.button_timkiem.Text = "Tìm kiếm";
+            this.button_timkiem.UseVisualStyleBackColor = true;
+            // 
+            // button_xoa
+            // 
+            this.button_xoa.Location = new System.Drawing.Point(278, 14);
+            this.button_xoa.Name = "button_xoa";
+            this.button_xoa.Size = new System.Drawing.Size(75, 23);
+            this.button_xoa.TabIndex = 4;
+            this.button_xoa.Text = "Xóa";
+            this.button_xoa.UseVisualStyleBackColor = true;
+            // 
+            // button_sửa
+            // 
+            this.button_sửa.Location = new System.Drawing.Point(152, 14);
+            this.button_sửa.Name = "button_sửa";
+            this.button_sửa.Size = new System.Drawing.Size(75, 23);
+            this.button_sửa.TabIndex = 3;
+            this.button_sửa.Text = "Sửa";
+            this.button_sửa.UseVisualStyleBackColor = true;
+            // 
+            // button_them
+            // 
+            this.button_them.Location = new System.Drawing.Point(33, 15);
+            this.button_them.Name = "button_them";
+            this.button_them.Size = new System.Drawing.Size(75, 23);
+            this.button_them.TabIndex = 2;
+            this.button_them.Text = "Thêm";
+            this.button_them.UseVisualStyleBackColor = true;
+            // 
+            // button_huongdan
+            // 
+            this.button_huongdan.Location = new System.Drawing.Point(660, 13);
+            this.button_huongdan.Name = "button_huongdan";
+            this.button_huongdan.Size = new System.Drawing.Size(75, 23);
+            this.button_huongdan.TabIndex = 19;
+            this.button_huongdan.Text = "Hướng dẫn";
+            this.button_huongdan.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -266,6 +277,7 @@
         private System.Windows.Forms.Button button_sửa;
         private System.Windows.Forms.Button button_them;
         private System.Windows.Forms.Button button_luu;
+        private System.Windows.Forms.Button button_huongdan;
     }
 }
 
