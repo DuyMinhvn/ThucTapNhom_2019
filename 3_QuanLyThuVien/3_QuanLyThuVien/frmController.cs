824ad57fe88a6e9e84668793e83b04b8ffc326b0
pham le huy <unknownsol98@gmail.com>
2019-09-21 22:35:58 +0700

    Bài 3: Tạo Form đăng ký

---
+++ 3_QuanLyThuVien/3_QuanLyThuVien/frmController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3_QuanLyThuVien
+{
+    class frmController
+    {
+        public static void show_panel(Panel pnl, bool state)
+        {
+            pnl.Visible = state;
+            foreach (Control VARIABLE in pnl.Controls)
+            {
+                VARIABLE.Visible = state;
+            }
+        }
+    }
+}
---
+++ 3_QuanLyThuVien/3_QuanLyThuVien/frmLogin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3_QuanLyThuVien
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            frmController.show_panel( pnlDangNhap, false);
+            frmController.show_panel( pnlDangKy, true);
+        }
+    }
+}
