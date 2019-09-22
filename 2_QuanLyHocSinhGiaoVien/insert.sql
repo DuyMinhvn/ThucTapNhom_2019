04f046b01ecb742d83bd7146206a2f2dbd1f014a
DuyMinhvn <cr7.duyminh1st@gmail.com>
2019-09-22 16:51:44 +0700

    add data giaovien

---
+++ 2_QuanLyHocSinhGiaoVien/insert.sql
@@ -0,0 +1,16 @@
+﻿--insert giaovien
+INSERT INTO dbo.GiaoVien( magiaovien, hovaten, ngaysinh, gioitinh, tongiao, quequan, diachithuongtru,sodienthoai,hocvi,chucvu,chuyenmon,tinhtrang,luong)
+VALUES
+(   'GV001',N'Nguyễn Văn Anh', GETDATE(),N'Nam',N'Kinh',N'Hà Nội',N'Hà Nội',N'0356609456',N'Thạc Sĩ',N'Giáo Viên',N'Toán',N'abc',N'1000000')
+INSERT INTO dbo.GiaoVien( magiaovien, hovaten, ngaysinh, gioitinh, tongiao, quequan, diachithuongtru,sodienthoai,hocvi,chucvu,chuyenmon,tinhtrang,luong)
+VALUES
+(   'GV002',N'Nguyễn Hoàng Long', GETDATE(),N'Nam',N'Kinh',N'Hà Nội',N'Hà Nội',N'0356609456',N'Thạc Sĩ',N'Giáo Viên',N'Lý',N'abc',N'1000000')
+INSERT INTO dbo.GiaoVien( magiaovien, hovaten, ngaysinh, gioitinh, tongiao, quequan, diachithuongtru,sodienthoai,hocvi,chucvu,chuyenmon,tinhtrang,luong)
+VALUES
+(   'GV003',N'Nguyễn Duy Minh', GETDATE(),N'Nam',N'Kinh',N'Hà Nội',N'Hà Nội',N'0356609456',N'Thạc Sĩ',N'Giáo Viên',N'Hóa',N'abc',N'1000000')
+INSERT INTO dbo.GiaoVien( magiaovien, hovaten, ngaysinh, gioitinh, tongiao, quequan, diachithuongtru,sodienthoai,hocvi,chucvu,chuyenmon,tinhtrang,luong)
+VALUES
+(   'GV004',N'Phạm Lê Huy', GETDATE(),N'Nữ',N'Tày',N'Hà Nội',N'Hà Nội',N'0356609456',N'Thạc Sĩ',N'Giáo Viên',N'Anh',N'abc',N'1000000')
+INSERT INTO dbo.GiaoVien( magiaovien, hovaten, ngaysinh, gioitinh, tongiao, quequan, diachithuongtru,sodienthoai,hocvi,chucvu,chuyenmon,tinhtrang,luong)
+VALUES
+(   'GV005',N'Bùi Mạnh Hoài', GETDATE(),N'Nam',N'Kinh',N'Hà Nội',N'Hà Nội',N'0356609456',N'Thạc Sĩ',N'Giáo Viên',N'Lịch Sử',N'abc',N'1000000')
