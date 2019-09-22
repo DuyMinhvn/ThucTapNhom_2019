1e9791a8bac1316bc7e2cfcb07dd99f232710b23
DuyMinhvn <cr7.duyminh1st@gmail.com>
2019-09-22 10:32:45 +0700

    uploat lai
    update

--- 1_QuanLyNhanSu/1_QuanLyNhanSu/Program.cs
+++ 1_QuanLyNhanSu/1_QuanLyNhanSu/Program.cs
@@ -16,7 +16,7 @@ namespace _1_QuanLyNhanSu
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMain());
         }
     }
 }
---
+++ 1_QuanLyNhanSu/1_QuanLyNhanSu/frmMain.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1_QuanLyNhanSu
+{
+    public partial class frmMain : Form
+    {
+        QLNSEntities obj = new QLNSEntities();
+        public frmMain()
+        {
+            //using (var context = new QLNSEntities()) {
+                var model = (from nv in obj.NhanViens
+                             join ct in obj.CongTacs
+                             on nv.MaNhanVien equals ct.MaNhanVien
+                             join cv in obj.ChucVus on ct.MaChucVu equals cv.MaChucVu
+                             join pb in obj.PhongBans on ct.MaPhongBan equals pb.MaPhongBan
+                             join td in obj.TrinhDoHocVans on nv.MaTrinhDoHocVan equals td.MaTrinhDoHocVan
+                             select new
+                             {
+                                 MaNhanVien = nv.MaNhanVien,
+                                 HoTen = nv.HoTen,
+                                 DanToc = nv.DanToc,
+                                 GioiTinh = nv.GioiTinh,
+                                 SDT = nv.SDT,
+                                 QueQuan = nv.QueQuan,
+                                 NgaySinh = nv.NgaySinh,
+                                 TenPhongBan = pb.TenPhongBan,
+                                 TenChucVu = cv.TenChucVu,
+                                 Luong = ct.Luong,
+                                 TenTDHv = td.TenTrinhDoHocVan
+
+                             });
+            //}
+                         
+            InitializeComponent();
+            dataGridView_nhanvien.DataSource = model.ToList();
+            //dataGridView_nhanvien.Columns[8].Visible = false;
+            //dataGridView_nhanvien.Columns[9].Visible = false;
+            //textBox_hoten.Enabled = false;
+            //textBox_dantoc.Enabled = false;
+            //textBox_sdt.Enabled = false;
+            //textBox_diachi.Enabled = false;
+            //comboBox_gioitinh.Enabled = false;
+            //dateTimePicker_ngaysinh.Enabled = false;
+            
+        }
+
+        private void themNhanvien(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            textBox_ma.Enabled = false;
+            textBox_hoten.Enabled = false;
+            textBox_dantoc.Enabled = false;
+            textBox_diachi.Enabled = false;
+            textBox_sdt.Enabled = false;
+            comboBox_gioitinh.Enabled = false;
+            dateTimePicker_ngaysinh.Enabled = false;
+            textBox_Luong.Enabled = false;
+            comboBox_cv.Enabled = false;
+            comboBox_pb.Enabled = false;
+            comboBox_td.Enabled = false;
+            comboBox_ma_pb.Enabled = false;
+            comboBox_ma_td.Enabled = false;
+            comboBox_ma_cv.Enabled = false;
+        }
+
+        private void dataGridView_nhanvien_CellClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView_nhanvien.Rows.Count > 0)
+            {
+                foreach (DataGridViewRow row in dataGridView_nhanvien.SelectedRows)
+                {
+                    textBox_ma.Text = row.Cells[0].Value.ToString();
+                    textBox_hoten.Text = row.Cells[1].Value.ToString();
+                    textBox_dantoc.Text = row.Cells[2].Value.ToString();
+                    comboBox_gioitinh.Text = row.Cells[3].Value.ToString();
+                    textBox_sdt.Text = row.Cells[4].Value.ToString();
+                    textBox_diachi.Text = row.Cells[5].Value.ToString();
+                    dateTimePicker_ngaysinh.Text = row.Cells[6].Value.ToString();
+                    comboBox_pb.Text = row.Cells[7].Value.ToString();
+                    textBox_Luong.Text = row.Cells[9].Value.ToString();
+                    comboBox_cv.Text = row.Cells[8].Value.ToString();
+                    comboBox_td.Text = row.Cells[10].Value.ToString();
+                    //textBox_hoten.Text = row.Cells[0].Value.ToString();
+                }
+            }
+        }
+        
+        private void textBox_Cv_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button_sửa_Click(object sender, EventArgs e)
+        {
+            textBox_ma.Enabled = false;
+            textBox_hoten.Enabled = true;
+            textBox_dantoc.Enabled = true;
+            textBox_diachi.Enabled = true;
+            textBox_sdt.Enabled = true;
+            comboBox_gioitinh.Enabled = true;
+            dateTimePicker_ngaysinh.Enabled = true;
+            textBox_Luong.Enabled = true;
+            comboBox_cv.Enabled =false;
+            comboBox_pb.Enabled = false;
+            comboBox_td.Enabled = false;
+            comboBox_ma_pb.Enabled = true;
+            comboBox_ma_td.Enabled = true;
+            comboBox_ma_cv.Enabled = true;
+            //hien lua chon o combobox ten phong ban
+            comboBox_pb.DataSource = obj.PhongBans.ToList();
+            comboBox_pb.DisplayMember = "TenPhongBan" ;
+            comboBox_pb.ValueMember = "MaPhongBan";
+            //hien thi lua chon o combobox chuc vu
+            comboBox_cv.DataSource = obj.ChucVus.ToList();
+            comboBox_cv.DisplayMember = "TenChucVu";
+            comboBox_cv.ValueMember = "MaChucVu";
+            //hien thi lua chon o combobox trinh do hoc van
+            comboBox_td.DataSource = obj.TrinhDoHocVans.ToList();
+            comboBox_td.DisplayMember = "TenTrinhDoHocVan";
+            comboBox_td.ValueMember = "MaTrinhDoHocVan";
+            //hien thi lua chon o combobox ma pb
+            comboBox_ma_pb.DataSource = obj.PhongBans.ToList();
+            comboBox_ma_pb.DisplayMember = "MaPhongBan" ;
+            comboBox_ma_pb.ValueMember = "MaPhongBan";
+            //hien thi lua chon o combobox ma cv
+            comboBox_ma_cv.DataSource = obj.ChucVus.ToList();
+            comboBox_ma_cv.DisplayMember = "MaChucVu";
+            comboBox_ma_cv.ValueMember = "MaChucVu";
+            //hien thi lua chon o combobox ma trinh do
+            comboBox_ma_td.DataSource = obj.TrinhDoHocVans.ToList();
+            comboBox_ma_td.DisplayMember = "MaTrinhDoHocVan";
+            comboBox_ma_td.ValueMember = "MaTrinhDoHocVan";
+            //update du lieu den tat ca cac bang
+            var nv = obj.NhanViens.First<NhanVien>();
+            nv.HoTen = textBox_hoten.Text;
+            nv.DanToc = textBox_dantoc.Text;
+            nv.SDT = textBox_sdt.Text;
+            nv.GioiTinh = comboBox_gioitinh.Text;
+            nv.NgaySinh = DateTime.Parse(dateTimePicker_ngaysinh.Text);
+            nv.QueQuan = textBox_diachi.Text;
+            nv.MaTrinhDoHocVan = comboBox_ma_td.Text;
+            var ct = obj.CongTacs.First<CongTac>();
+            ct.MaChucVu = comboBox_ma_cv.Text;
+            ct.MaPhongBan = comboBox_ma_pb.Text;
+            obj.SaveChanges();
+            
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void exitToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
