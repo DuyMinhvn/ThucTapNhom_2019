46f3605993f77add5df4e86f6c5acb541d4593ac
hoang98long <hoang98long@gmail.com>
2019-10-02 09:24:48 +0700

    cmmit

--- 2_QuanLyHocSinhGiaoVien/QuanLyHSGV/QLHSGV.Designer.cs
+++ 2_QuanLyHocSinhGiaoVien/QuanLyHSGV/QLHSGV.Designer.cs
@@ -29,11 +29,11 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button_load = new System.Windows.Forms.Button();
             this.button_luu = new System.Windows.Forms.Button();
             this.button_xoa = new System.Windows.Forms.Button();
             this.button_sua = new System.Windows.Forms.Button();
             this.button_Them = new System.Windows.Forms.Button();
-            this.button_load = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.groupBox1.SuspendLayout();
@@ -48,64 +48,74 @@
             this.groupBox1.Controls.Add(this.button_xoa);
             this.groupBox1.Controls.Add(this.button_sua);
             this.groupBox1.Controls.Add(this.button_Them);
-            this.groupBox1.Location = new System.Drawing.Point(13, 3);
+            this.groupBox1.Location = new System.Drawing.Point(10, 2);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(1008, 262);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(756, 213);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
             // 
+            // button_load
+            // 
+            this.button_load.Location = new System.Drawing.Point(688, 167);
+            this.button_load.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button_load.Name = "button_load";
+            this.button_load.Size = new System.Drawing.Size(56, 19);
+            this.button_load.TabIndex = 9;
+            this.button_load.Text = "Load";
+            this.button_load.UseVisualStyleBackColor = true;
+            // 
             // button_luu
             // 
-            this.button_luu.Location = new System.Drawing.Point(918, 162);
+            this.button_luu.Location = new System.Drawing.Point(688, 128);
+            this.button_luu.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button_luu.Name = "button_luu";
-            this.button_luu.Size = new System.Drawing.Size(75, 23);
+            this.button_luu.Size = new System.Drawing.Size(56, 19);
             this.button_luu.TabIndex = 8;
             this.button_luu.Text = "Lưu";
             this.button_luu.UseVisualStyleBackColor = true;
             // 
             // button_xoa
             // 
-            this.button_xoa.Location = new System.Drawing.Point(918, 112);
+            this.button_xoa.Location = new System.Drawing.Point(688, 91);
+            this.button_xoa.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button_xoa.Name = "button_xoa";
-            this.button_xoa.Size = new System.Drawing.Size(75, 23);
+            this.button_xoa.Size = new System.Drawing.Size(56, 19);
             this.button_xoa.TabIndex = 7;
             this.button_xoa.Text = "Xóa";
             this.button_xoa.UseVisualStyleBackColor = true;
+            this.button_xoa.Click += new System.EventHandler(this.xoaGV);
             // 
             // button_sua
             // 
-            this.button_sua.Location = new System.Drawing.Point(918, 65);
+            this.button_sua.Location = new System.Drawing.Point(688, 55);
+            this.button_sua.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button_sua.Name = "button_sua";
-            this.button_sua.Size = new System.Drawing.Size(75, 23);
+            this.button_sua.Size = new System.Drawing.Size(56, 19);
             this.button_sua.TabIndex = 6;
             this.button_sua.Text = "Sửa";
             this.button_sua.UseVisualStyleBackColor = true;
             // 
             // button_Them
             // 
-            this.button_Them.Location = new System.Drawing.Point(918, 21);
+            this.button_Them.Location = new System.Drawing.Point(688, 17);
+            this.button_Them.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button_Them.Name = "button_Them";
-            this.button_Them.Size = new System.Drawing.Size(75, 23);
+            this.button_Them.Size = new System.Drawing.Size(56, 19);
             this.button_Them.TabIndex = 5;
             this.button_Them.Text = "Thêm";
             this.button_Them.UseVisualStyleBackColor = true;
             // 
-            // button_load
-            // 
-            this.button_load.Location = new System.Drawing.Point(918, 205);
-            this.button_load.Name = "button_load";
-            this.button_load.Size = new System.Drawing.Size(75, 23);
-            this.button_load.TabIndex = 9;
-            this.button_load.Text = "Load";
-            this.button_load.UseVisualStyleBackColor = true;
-            // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.dataGridView1);
-            this.groupBox2.Location = new System.Drawing.Point(12, 271);
+            this.groupBox2.Location = new System.Drawing.Point(9, 220);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(1009, 248);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Size = new System.Drawing.Size(757, 202);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "groupBox2";
@@ -114,19 +124,21 @@
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(3, 18);
+            this.dataGridView1.Location = new System.Drawing.Point(2, 15);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(1003, 227);
+            this.dataGridView1.Size = new System.Drawing.Size(753, 185);
             this.dataGridView1.TabIndex = 0;
             // 
             // QLHSGV
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1033, 531);
+            this.ClientSize = new System.Drawing.Size(775, 431);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "QLHSGV";
             this.Text = "QLHSGV";
             this.groupBox1.ResumeLayout(false);
